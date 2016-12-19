d47e144d2c0e3886445d8d6d4b0654044e94b3fe
Tom Emmerson <thomasemmerson@users.noreply.github.com>
2016-12-19 16:35:50 +0000

    Latest commit

---
+++ game_test.js
@@ -0,0 +1,22 @@
+//import chai expectations
+var expect = require('chai').expect;
+
+describe('GAME INSTANCE FUNCTIONS', function () {
+	describe('checkGameStatus', function () {
+		var checkGameStatus = require('../game_logic/game_instance.js').checkGameStatus;
+		it('should tell me when the game is over', function () {
+			var players = [
+				{
+					ships: [
+						{
+							locations: [[0,0]],
+							damage: [[0,0]]
+						}
+					]
+				}
+			];
+			var actual = checkGameStatus(players);
+			expect(actual).to.be.false;
+		});
+	});
+});
